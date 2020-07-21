df40c6cbc32e7a197ce22cbfa6fe41bb8bfb2a48
Your Name <frogmouth@unzud.com>
2020-07-21 18:09:43 +1000

    test commit

--- kyria-refactored/rules.mk
+++ kyria-refactored/rules.mk
@@ -3,7 +3,7 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = yes        # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no       # Enable the mousekey feature
-
+ 
 SRC += shortcuts.c
 
 ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
