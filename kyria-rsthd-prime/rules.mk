535d78e3819cb7db5c8f9d6a498b407fd85a6c08
frogmOuth <frogmouth@unzud.com>
2020-11-25 23:14:56 +1100

    Added combo support, removed other features to make space

--- kyria-rsthd-prime/rules.mk
+++ kyria-rsthd-prime/rules.mk
@@ -1,10 +1,11 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 LEADER_ENABLE = no         # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no       # Enable the inbuilt mouse key feature
 TAP_DANCE_ENABLE = no      # Enable tap-dance (NB also uncomment #define NO_ACTION_TAPPING in config.h)
 WPM_ENABLE = no		       # Enable simple WPM display
+COMBO_ENABLE = yes	       # Enable combo (chording) functionality
 
 # COMMAND_ENABLE = no
 # EXTRAKEY_ENABLE = no
@@ -15,12 +16,13 @@ OS_SHORTCUTS = yes	    # Enable OS shortcut mapping
 CUSTOM_EDIT = yes	    # Enable custom editing keys
 CUSTOM_MOUSE = yes	    # Enable custom mouse keys
 LAYER_TAP_TOGGLE = yes	# Enable the layer-tap-toggle feature
-COMPOSE_KEY = yes	    # Enable the custom compose key feature
+COMPOSE_KEY = no	    # Enable the custom compose key feature
 
 # Uncomment one of the next lines to prevent unused variable/function errors
 # in qmk_firmware/quantum/process_keycode/process_rgb.c from halting the compile
 #EXTRAFLAGS += -Wno-unused-function -Wno-unused-variable
 EXTRAFLAGS += -Wno-error=unused-variable -Wno-error=unused-function
+
 
 # Don't edit from here down
 
@@ -40,6 +42,10 @@ endif
 
 ifeq ($(strip $(LEADER_ENABLE)), yes)
 	SRC += leader.c
+endif
+
+ifeq ($(strip $(COMBO_ENABLE)), yes)
+	SRC += combos.c
 endif
 
 ifeq ($(strip $(OS_SHORTCUTS)), yes)
