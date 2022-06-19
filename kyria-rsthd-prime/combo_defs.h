76ee305146b92439420b0316bcad1e4c966cb614
username <frogmouth@unzud.com>
2022-06-20 00:59:25 +1000

    Added comborolls feature, revised all combos

---
+++ kyria-rsthd-prime/combo_defs.h
@@ -0,0 +1,111 @@
+
+/* Copyright 2020 @frogm0uth
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This file contains the definitions of combos. This includes both QMK combos
+ * and user-space comborolls. Comborolls are triggered by a sequence of
+ * keypresses instead of pressing both keys at the same time as with combos.
+ *
+ * QMK combos support any number of trigger keys. Comborolls support only two
+ * keys.
+ *
+ * The macros are named (CMBO|LtoR|RtoL|LRRL )_(KEY|ARR|LIT|STR ). To
+ * use the CMBO_* macros, set COMBO_ENABLE=yes on rule.mk. To use the
+ * other (comboroll) macros, set COMBOROLL_ENABLE=yes in rules.mk.
+ *
+ * CMBO: generates normal QMK combo
+ * LtoR: comboroll, triggers pressed from left to right
+ * RtoL: comboroll, triggers pressed from right to left
+ * LRRL: comboroll, triggers pressed in either direction
+ *
+ * KEY: arguments are name, keycode, triggers
+ * ARR: arguments are name, array of keycodes, triggers
+ * LIT: arguments are name, literal string, triggers
+ * STR: arguments are name, triggers. The combo output is the name.
+ *
+ * Note: by convention, the trigger keys should be listed from left to right
+ * location in the keymap (where it makes sense to do so ).
+ */
+// clang-format off
+
+// Anti-SFU, mostly vertical combos
+LRRL_STR( sc, KC_S, KC_C )
+LRRL_STR( ps, KC_S, KC_P )
+LtoR_STR( sp, KC_P, KC_G )
+LRRL_STR( tw, KC_T, KC_W )  
+LRRL_STR( ui, KC_U, KC_I )
+
+// Anti-pinballing
+RtoL_STR( er,  KC_O,    KC_A )
+RtoL_STR( es,  KC_O,    CU_RSFT )
+RtoL_STR( ed,  CU_COMM, CU_RSFT )
+
+// Awkward bigrams/trigrams
+LtoR_STR( qu,  KC_C, KC_W )
+LtoR_STR( cr,  KC_W, KC_F )
+LtoR_STR( pr,  KC_G, KC_D )
+RtoL_STR( my,  KC_M, CU_DOT )
+LtoR_STR( min, KC_P, KC_N )
+
+// Inner column minimization
+LtoR_STR( br,  KC_R, KC_H )
+LtoR_STR( ble, KC_T, KC_D )
+LRRL_STR( be,  KC_P, CU_COMM )
+LRRL_STR( but, KC_G, CU_COMM )
+LRRL_STR( by,  KC_D, CU_COMM )
+
+// Alternate punctuation on left hand
+LtoR_KEY( comma,  KC_COMM, CU_LSFT, KC_P )
+LtoR_KEY( period, KC_DOT,  CU_LSFT, KC_G )
+
+LtoR_KEY( caps, KC_X,  CU_LSFT, KC_D )
+
+// Common word endings, right hand
+RtoL_STR( ould, CU_DOT, KC_MINS )
+LRRL_STR( ough, KC_U,   KC_MINS )
+
+LRRL_STR( ing,  KC_I, CU_COMM )
+LRRL_STR( ight, KC_I, CU_DOT )
+
+RtoL_STR( ally, KC_L, CU_RSFT )
+LRRL_STR( ess,  KC_Y, CU_RSFT )
+
+// Common words
+RtoL_STR( you,  KC_U, CU_DOT )
+
+LRRL_STR( very, KC_V, KC_Y )
+LRRL_STR( ever, KC_E, CU_COMM )
+
+LRRL_STR( was,  KC_W, KC_Y )
+LRRL_STR( his,  KC_H, KC_Y )
+
+// Utilities
+LRRL_KEY( search,   SC_SEARCH,      KC_SPC,  KC_ENT )       // System search box, right thumb
+LRRL_KEY( capslock, KC_CAPS,        KC_E,    CL_SYMS )      // Capslock, left thumb
+LtoR_ARR( esc_x,                                            // Escape-X for emacs
+        ARRAY_PROTECT( 
+            KC_ESC,
+            KC_X
+     ),
+    KC_V, KC_C )
+
+// Syntax layer
+LRRL_LIT( php, "<?php", KC_QUES, KC_LBRC )
+
+// Window snapping
+LRRL_KEY( snaptop,    SC_SNAP_TOP,    SC_SNAP_TOPLEFT,    SC_SNAP_TOPRIGHT )       // Top two quadrants = snap top
+LRRL_KEY( snapbottom, SC_SNAP_BOTTOM, SC_SNAP_BOTTOMLEFT, SC_SNAP_BOTTOMRIGHT )    // Bottom two quadrants = snap bottom
--- kyria-rsthd-prime/combos.c
+++ kyria-rsthd-prime/combos.c
@@ -15,119 +15,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-
-/** Combo definitions. NB don't edit here, edit in combos.def.
+/** Combo definitions. NB don't edit here, edit in combo_defs.h. See further down
+ * this file for comboroll macros/code.
  *
- * Code borrowed/adapted from :
+ * Macros for combos adapted from:
  *
  * https://github.com/sevanteri/qmk_firmware/blob/9cca95785e6fd5001367d02fceddae628f6d48c9/users/sevanteri/combos.h
  * https://github.com/qmk/qmk_firmware/blob/master/keyboards/gboards/g/keymap_combo.h
  */
 
-
 #include "config.h"
 #include "keymap.h"
-
-#define ARRAY_PROTECT(...) __VA_ARGS__ 
-
-#define CMB_NUL(...)
-
-#define COMBO_KEY_ENUM(name, keycode, ...)  COMBO_ID_##name,
-#define COMBO_KEY_DATA(name, keycode, ...)  const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END};
-#define COMBO_KEY_MAP(name, keycode, ...)  [COMBO_ID_##name] = COMBO(COMBO_SEQ_##name, keycode),
-#define COMBO_KEY_EXEC(name, keycode, ...)
-
-
-#define COMBO_ARR_ENUM(name, protected_array, ...)  \
-    COMBO_ID_##name,
-
-#define COMBO_ARR_DATA(name, protected_array, ...)  \
-    const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END}; \
-    const uint16_t combo_array_##name[] = { protected_array, KC_NO };
-
-#define COMBO_ARR_MAP(name, protected_array, ...)  \
-    [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
-
-#define COMBO_ARR_EXEC(name, protected_array, ...) \
-    case COMBO_ID_##name: \
-        process_combo_array(combo_array_##name); \
-    break;
-
-
-#define COMBO_STR_ENUM(name, ...)  \
-    COMBO_ID_##name,
-
-#define COMBO_STR_DATA(name, ...)  \
-    const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END};
-
-#define COMBO_STR_MAP(name, ...)  \
-    [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
-
-#define COMBO_STR_EXEC(name, ...) \
-    case COMBO_ID_##name: \
-        process_combo_string(#name); \
-    break;
-
-
-#define COMBO_LIT_ENUM(name, string_literal, ...)    \
-    COMBO_ID_##name,
-
-#define COMBO_LIT_DATA(name, string_literal, ...)                        \
-    const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END};
-
-#define COMBO_LIT_MAP(name, string_literal, ...)         \
-    [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
-
-#define COMBO_LIT_EXEC(name, string_literal, ...)       \
-    case COMBO_ID_##name: \
-        process_combo_string(string_literal); \
-    break;
-
-
-void process_combo_array(const uint16_t *keyptr);
-void process_combo_string(const char* str);
-
-    
-// Enum for combo codes
-#undef CMB_KEY
-#undef CMB_ARR
-#undef CMB_STR
-#undef CMB_LIT
-#define CMB_KEY COMBO_KEY_ENUM
-#define CMB_ARR COMBO_ARR_ENUM
-#define CMB_STR COMBO_STR_ENUM
-#define CMB_LIT COMBO_LIT_ENUM
-enum combo_events {
-#include "combos.def"
-    COMBO_LENGTH
-};
-uint16_t COMBO_LEN = COMBO_LENGTH;
-
-// Define combo key sequences in memory
-#undef CMB_KEY
-#undef CMB_ARR
-#undef CMB_STR
-#undef CMB_LIT
-#define CMB_KEY COMBO_KEY_DATA
-#define CMB_ARR COMBO_ARR_DATA
-#define CMB_STR COMBO_STR_DATA
-#define CMB_LIT COMBO_LIT_DATA
-#include "combos.def"
-
-// Define mapping from key sequences to combo codes or actions
-#undef CMB_KEY
-#undef CMB_ARR
-#undef CMB_STR
-#undef CMB_LIT
-#define CMB_KEY COMBO_KEY_MAP
-#define CMB_ARR COMBO_ARR_MAP
-#define CMB_STR COMBO_STR_MAP
-#define CMB_LIT COMBO_LIT_MAP
-combo_t key_combos[] = {
-#include "combos.def"
-};
-
 
 // Emit an array of keycodes
 void process_combo_array(const uint16_t *keyptr) {
@@ -137,7 +35,7 @@ void process_combo_array(const uint16_t *keyptr) {
 }
 
 // Emit a string, clear shift after first character
-void process_combo_string(const char* str) {
+void process_combo_string(const char *str) {
     send_char(*str++);
     clear_mods();
     while (*str) {
@@ -145,24 +43,320 @@ void process_combo_string(const char* str) {
     }
 }
 
+// Register a single key. Handles custom keycodes.
+void register_combo_key(uint16_t keycode, keyrecord_t *record) {
+    if (keycode > SAFE_RANGE) { // handle custom keycodes, a bit iffy but seems to work...
+        record->event.pressed = true;
+        process_record_user_character(keycode, record);
+    } else {
+        if (keycode == KC_CAPS) { // needs special treatment!??
+            tap_code16(keycode);
+        } else {
+            register_code16(keycode);
+        }
+    }
+}
+
+// Unregister a single key. Handles custom keycodes.
+void unregister_combo_key(uint16_t keycode, keyrecord_t *record) {
+    if (keycode > SAFE_RANGE) { // handle custom keycodes, a bit iffy but seems to work...
+        record->event.pressed = false;
+        process_record_user_character(keycode, record);
+    } else {
+        unregister_code16(keycode);
+    }
+}
+
+// Tap a single key. Handles custom keycodes.
+void tap_combo_key(uint16_t keycode, keyrecord_t *record) {
+    register_combo_key(keycode, record);
+    unregister_combo_key(keycode, record);
+}
+
+// clang-format off
+#undef  ARRAY_PROTECT
+#define ARRAY_PROTECT(...) __VA_ARGS__ 
+
+
+// Empty defs for comborolls
+#define LtoR_KEY(name, out, k1, k2)
+#define LtoR_ARR(name, out, k1, k2)
+#define LtoR_LIT(name, out, k1, k2)
+#define LtoR_STR(name,      k1, k2)
+
+#define RtoL_KEY(name, out, k1, k2)
+#define RtoL_ARR(name, out, k1, k2)
+#define RtoL_LIT(name, out, k1, k2)
+#define RtoL_STR(name,      k1, k2)
+
+#define LRRL_KEY(name, out, k1, k2)
+#define LRRL_ARR(name, out, k1, k2)
+#define LRRL_LIT(name, out, k1, k2)
+#define LRRL_STR(name,      k1, k2)
+
+/**
+ * This section used only if QMK combos are enabled
+ */
+#if defined(COMBO_ENABLE)
+
+// Enum for combo codes
+#define CMBO_KEY(name, out, ...)  COMBO_ID_##name,
+#define CMBO_ARR(name, out, ...)  COMBO_ID_##name,
+#define CMBO_LIT(name, out, ...)  COMBO_ID_##name,
+#define CMBO_STR(name, ...)       COMBO_ID_##name,
+
+enum combo_events {
+#   include "combo_defs.h"
+    COMBO_LENGTH
+};
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+
+// Define combo key sequences in memory
+#undef  CMBO_KEY
+#undef  CMBO_ARR
+#undef  CMBO_STR
+#undef  CMBO_LIT
+
+#define CMBO_DATA(name, out, ...)  const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END};
+
+#define CMBO_KEY(name, out, ...)  CMBO_DATA(name, out, __VA_ARGS__)  
+#define CMBO_ARR(name, out, ...)  const uint16_t PROGMEM COMBO_SEQ_##name[] = {__VA_ARGS__, COMBO_END}; \
+                                  const uint16_t combo_array_##name[] = { out, KC_NO };
+#define CMBO_LIT(name, out, ...)  CMBO_DATA(name, out, __VA_ARGS__)  
+#define CMBO_STR(name, ...)       CMBO_DATA(name, #name, __VA_ARGS__)  
+
+#include "combo_defs.h"
+
+
+// Define mapping from key sequences to combo codes or actions
+#undef  CMBO_KEY
+#undef  CMBO_ARR
+#undef  CMBO_STR
+#undef  CMBO_LIT
+
+#define CMBO_KEY(name, out, ...)  [COMBO_ID_##name] = COMBO(COMBO_SEQ_##name, out),
+#define CMBO_ARR(name, out, ...)  [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
+#define CMBO_STR(name, out, ...)  [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
+#define CMBO_LIT(name, ...)       [COMBO_ID_##name] = COMBO_ACTION(COMBO_SEQ_##name),
+
+combo_t key_combos[] = {
+#   include "combo_defs.h"
+};
+
 
 // Execute code for action combos
-#undef CMB_KEY
-#undef CMB_ARR
-#undef CMB_STR
-#undef CMB_LIT
-#define CMB_KEY COMBO_KEY_EXEC
-#define CMB_ARR COMBO_ARR_EXEC
-#define CMB_STR COMBO_STR_EXEC
-#define CMB_LIT COMBO_LIT_EXEC
+#undef  CMBO_KEY
+#undef  CMBO_ARR
+#undef  CMBO_STR
+#undef  CMBO_LIT
+
+#define CMBO_KEY(name, out, ...)
+#define CMBO_ARR(name, out, ...)  case COMBO_ID_##name: process_combo_array(combo_array_##name); break;
+#define CMBO_LIT(name, out, ...)  case COMBO_ID_##name: process_combo_string(out); break;
+#define CMBO_STR(name, ...)       case COMBO_ID_##name: process_combo_string(#name); break;
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
     uint8_t mods = get_mods();
 
     if (pressed) {
         switch(combo_index) {
-#include "combos.def"
+#         include "combo_defs.h"
         }
     }
     set_mods(mods);
 }
+
+#endif // COMBO_ENABLE
+// clang-format on
+
+/** Comboroll data/functions.  This is my userspace implementation of either
+ * (depending on how you look at it) combos or Alan Reiser's "adaptive keys."
+ * Compared to QMK combos, the order of trigger keys pressed matters, only two
+ * trigger keys can be used, and the second one must be pressed before the first
+ * is released. Compared to adaptive keys, nothing is emitted until the second
+ * trigger key matches and there's no need for the emitted keys to have any
+ * relation to the trigger keys.
+ *
+ * Comborolls are defined by macros in combo_defs.h. There is a macro that
+ * defines the trigger keys in both directions for cases where the direction of
+ * the roll is not obvious, or for triggering from both hands or thumb keys when
+ * you can't roll.  The time allowed to trigger a comboroll is relatively short
+ * (between the QMK combo and tap-hold delay).
+ *
+ * I considered having the trigger sequence be more than two keys, but decided
+ * that it would be used rarely and the implementation with just two keys is
+ * simpler. For 3+ keys, use regular QMK combos.
+ */
+
+// clang-format off
+
+// Clear macros for QMK combos
+#undef  CMBO_KEY
+#undef  CMBO_ARR
+#undef  CMBO_STR
+#undef  CMBO_LIT
+
+#define CMBO_KEY(name, out, k1, k2)
+#define CMBO_ARR(name, out, k1, k2)
+#define CMBO_LIT(name, out, k1, k2)
+#define CMBO_STR(name,      k1, k2)
+
+
+/**
+ * This section included only if comborolls are enabled
+ */
+#if defined (COMBOROLL_ENABLE)
+
+// Define the data arrays for array output comborolls
+#undef  LtoR_ARR
+#undef  RtoL_ARR
+#undef  LRRL_ARR
+
+#define LtoR_ARR(name, out, k1, k2)    const uint16_t comboroll_array_##name[] = { out, KC_NO };
+#define RtoL_ARR(name, out, k1, k2)    const uint16_t comboroll_array_##name[] = { out, KC_NO };
+#define LRRL_ARR(name, out, k1, k2)    const uint16_t comboroll_array_##name[] = { out, KC_NO };
+
+#include "combo_defs.h"
+
+
+// Define the array of comboroll nodes
+#undef  LtoR_KEY
+#undef  LtoR_ARR
+#undef  LtoR_LIT
+#undef  LtoR_STR
+
+#undef  RtoL_KEY
+#undef  RtoL_ARR
+#undef  RtoL_LIT
+#undef  RtoL_STR
+
+#undef  LRRL_KEY
+#undef  LRRL_ARR
+#undef  LRRL_LIT
+#undef  LRRL_STR
+
+#define LtoR_KEY(name, out, k1, k2)     {comboroll_t_keycode, k1, k2, {.output_keycode=out}},
+#define LtoR_ARR(name, out, k1, k2)     {comboroll_t_array,   k1, k2, {.output_array=comboroll_array_##name}},
+#define LtoR_LIT(name, out, k1, k2)     {comboroll_t_string,  k1, k2, {.output_string=out}},
+#define LtoR_STR(name,      k1, k2)     {comboroll_t_string,  k1, k2, {.output_string=#name}},
+
+#define RtoL_KEY(name, out, k1, k2)     {comboroll_t_keycode, k2, k1, {.output_keycode=out}},
+#define RtoL_ARR(name, out, k1, k2)     {comboroll_t_array,   k2, k1, {.output_array=comboroll_array_##name}},
+#define RtoL_LIT(name, out, k1, k2)     {comboroll_t_string,  k2, k1, {.output_string=out}},
+#define RtoL_STR(name,      k1, k2)     {comboroll_t_string,  k2, k1, {.output_string=#name}},
+
+#define LRRL_KEY(name, out, k1, k2)     LtoR_KEY(name, out, k1, k2)   RtoL_KEY(name, out, k1, k2)
+#define LRRL_ARR(name, out, k1, k2)     LtoR_ARR(name, out, k1, k2)   RtoL_ARR(name, out, k1, k2)
+#define LRRL_LIT(name, out, k1, k2)     LtoR_LIT(name, out, k1, k2)   RtoL_LIT(name, out, k1, k2)
+#define LRRL_STR(name,      k1, k2)     LtoR_STR(name,      k1, k2)   RtoL_STR(name,      k1, k2)
+
+comboroll_t comborolls_data[] = {
+#   include "combo_defs.h"
+};
+
+// clang-format on
+
+// local vars
+static bool        is_in_comboroll  = false;
+static uint16_t    firstkey_matched = KC_NO;
+static keyrecord_t firstkey_record;
+static uint16_t    comboroll_timer = 0;
+
+// Produce output for the given comboroll
+void process_comboroll(comboroll_t *cr) {
+    switch (cr->type) {
+            // We have to do a tap for an output key. We can't register it, because we don't have a way to
+            // remember it to unregister later. This is why we can't have hold type keys as combo
+            // output. To be able to remember a held key, we would need a stack or list of active combos
+            // (or something along those lines), as we could activate a second hold key while the first is
+            // still active. Perhaps a single remembered combo would be enough, and any subsequent would
+            // be forced to be a tap.
+        case comboroll_t_keycode:
+            tap_combo_key(cr->output_keycode, &firstkey_record);
+            break;
+
+        case comboroll_t_array:
+            process_combo_array(cr->output_array);
+            break;
+
+        case comboroll_t_string:
+            process_combo_string(cr->output_string);
+            break;
+    }
+}
+
+// Scan for match on first key
+bool comboroll_scan_firstkey(uint16_t keycode) {
+    comboroll_t *cr = comborolls_data;
+    int          i;
+
+    for (i = 0; i < sizeof comborolls_data / sizeof *comborolls_data; cr++, i++) {
+        if (keycode == cr->firstkey) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Scan for match on second key
+comboroll_t *comboroll_scan_secondkey(uint16_t firstkey, uint16_t secondkey) {
+    comboroll_t *cr = comborolls_data;
+    int          i;
+
+    for (i = 0; i < sizeof comborolls_data / sizeof *comborolls_data; cr++, i++) {
+        if (firstkey == cr->firstkey && secondkey == cr->secondkey) {
+            return cr;
+        }
+    }
+    return NULL;
+}
+
+// Process comboroll.
+bool process_record_comboroll(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (is_in_comboroll) {
+            is_in_comboroll = false;
+            // Look for match on second key
+            comboroll_t *second = comboroll_scan_secondkey(firstkey_matched, keycode);
+            if (second) {
+                process_comboroll(second);                                // matched second key, so emit the combo
+                unregister_combo_key(firstkey_matched, &firstkey_record); // unregister first key prematurely to avoid hanging mods
+                return false;                                             // no further processing
+
+            } else {                                                    // no match
+                register_combo_key(firstkey_matched, &firstkey_record); // register the first key
+                                                                        // fall through to check for start of comboroll again
+            }
+        }
+        // If still here, check for a match as first key
+        if (comboroll_scan_firstkey(keycode)) {
+            is_in_comboroll  = true;
+            firstkey_record  = *record;
+            firstkey_matched = keycode; // QMK doesn't use record->keycode - ?
+            comboroll_timer  = timer_read();
+            return false; // no further processing as we've held the key
+        } else {
+            return true; // let QMK handle the key as normal
+        }
+    } else {
+        if (is_in_comboroll) {
+            // If we're here, the first key was just released. So send it and cancel the combo
+            tap_combo_key(firstkey_matched, &firstkey_record);
+            is_in_comboroll = false;
+            return false; // We already unregistered it in the tap, so QMK should leave it alone (?)
+        }
+    }
+    return true;
+}
+
+// Handle timing and output first key if too long for second. Must call from matrix_scan_user()
+void comboroll_tick() {
+    if (is_in_comboroll) {
+        if (timer_elapsed(comboroll_timer) > COMBOROLL_TIMEOUT) {
+            is_in_comboroll = false;
+            register_combo_key(firstkey_matched, &firstkey_record); // register the first key
+        }
+    }
+}
+#endif // COMBOROLL_ENABLE
