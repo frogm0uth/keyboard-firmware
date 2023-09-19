508e4272202bfc318b356a13e4ae6822ca055d49
frogm0uth <frogmouth@unzud.com>
2023-09-20 00:14:11 +1000

    Updated copyrights

--- common/util.h
+++ common/util.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 @frogm0uth
+/* Copyright 2020-2023 @frogm0uth
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
